f341711e785a2be2dccf015d7af21a1cc6d658da
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-04-26 15:03:36 -0700

    working version

--- KeyboardloggerScraper.py
+++ KeyboardloggerScraper.py
@@ -27,7 +27,7 @@ listWithDailyKeystrokes = []
 todaysDate = datetime.today().strftime('%Y-%m-%d')
 
 #if len(index)==0 set header to Date, sum other wise just read file
-df = pd.read_csv("KeystrokeRecords.csv")
+df = pd.read_csv("/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeRecords.csv")
 if(len(df)==0):
     df.columns = ["Date", "sum"]
 
@@ -43,10 +43,10 @@ def addDailyRow():
     print(df)
 
 def saveToCsv():
-    df.to_csv("KeystrokeRecords.csv", index=False)
+    df.to_csv("/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeRecords.csv", index=False)
 
 def getCharacterFromKeystrokeCollector():
-    with open("../LogData/KeystrokeCollector.txt", "r") as f:
+    with open("/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollector.txt", "r") as f:
         for i in f:
             listWithDailyKeystrokes.append(i.split(" ")[2].strip())
             #split at space
@@ -55,8 +55,6 @@ def getCharacterFromKeystrokeCollector():
 def addKeystrokes():
     #iterate over columns from daily row
     #count occurence of current cell object in listWithDailyKeystrokes and add this value to the current cell object
-     
-
     #4. if exist in header go to row of current day and increase amount um 1
     for i in listWithDailyKeystrokes:
         if i not in df:
@@ -78,10 +76,38 @@ def keyStrokeCounter():
     can just add this script to crontab. need to fix problem that to every row which is not last row all values are set to 0.
 
     STEPS
+
+        check if script works. i.e. 
+
+
         https://crontab.guru/#*_*_1_*_* for this script
         auslagern von dateien mit sensiblen daten von github
         transfer data from KeystrokeCollector.txt to KeystrokeCollectorBackup.txt
         delete data from KeystrokeCollector.txt
+        
+        execution diagram:
+            1. add row in KeystrokeRecords with daily date as an entry and rest zeros
+            2. get all daily characters 
+            3. if daily chracter does not appear in df add him in header and set whole column to 0
+            4. count occurence of each character from daily keystrokes and add this number to adequate column
+            5. transfer data from daily keystrokecollector to backup file 
+            6. delete daily keystroke collector 
+
+            Note:
+                it is important that this gets executed exactly in that order. the script gets executed every 24 hours. 
+                everything is fully functional. just need to test if daily date entry works. general does the execution with crontab work?
+
+                1. configure crontab for one minute
+                    check if entries are made  
+                2. set crontab to 24h
+                2. technically done. define next steps for finally completing the project.
+
+            Next steps: 
+                1. check if it worked
+                2. send me every sunday an mail with the diagram 
+
+
+        send every sunday email with diagram of keystrokes
     '''
     counter = 2
     for cell in df.iloc[-1,2:]:
@@ -92,9 +118,23 @@ def keyStrokeCounter():
         counter += 1
     #set sum
     df.iloc[-1, 1]=df.iloc[-1, 2:].sum()
+
+def KeyboardloggerDataTransfer():
+    #transfer data from keystrokeCollector to keystrokecollectorbackup 
+    #function needs to be last one which gets exectuted to ensure to dont delete before i make the analysis
+    with open('/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollector.txt','r') as firstfile, open('/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollectorBackup.txt','a') as secondfile:
+        for line in firstfile:   
+            secondfile.write(line)
+
+#daily deletion of daily collected keystrokes
+def KeyboardloggerDataDeletion():
+    open('/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollector.txt', 'w').close()
     
 addDailyRow()    
 getCharacterFromKeystrokeCollector()
 addKeystrokes()
 keyStrokeCounter()
 saveToCsv()
+KeyboardloggerDataTransfer()
+KeyboardloggerDataDeletion()
+

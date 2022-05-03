d5bcc0b3201f44b252db6185b289f3ab64d98733
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-05-03 06:40:42 +0200

    clean

--- KeyboardloggerScraper.py
+++ KeyboardloggerScraper.py
@@ -1,21 +1,3 @@
-'''
-1. should create csv file with every possible keyboardletter as an column header
-    1. create header sum and date
-    1. if row with current date does not exist (in csv) add row with current date and set every cell to 0
-    2. go in keystroke script, go through every line starting in first line. 
-    take character between '' 
-    getCharacterFromKeystrokeCollector()
-    4. if exist in header go to row of current day and increase amount um 1
-
-
-2. executes script every day 12:00 night synced with time zone
-    1. in header "daily sum" add sum of all numbers from todays row 
-    #should count occurence of every keyboard letter and than add occurrence to respective keyboard letter
-    2. take all the content from collector script and copy the content to txt file "KeystrokeArchive"
-    3 remove all the 
-    2. reset keystroke collector script (delete all its content)
-    I could reset the script also every day (store data in another script)
-'''
 #imports
 import os
 from datetime import datetime
@@ -49,39 +31,13 @@ def getCharacterFromKeystrokeCollector():
     with open("/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollector.txt", "r") as f:
         for i in f:
             listWithDailyKeystrokes.append(i.split(" ")[2].strip())
-            #split at space
-            #take third elem
 
 def addKeystrokes():
-    #iterate over columns from daily row
-    #count occurence of current cell object in listWithDailyKeystrokes and add this value to the current cell object
-    #4. if exist in header go to row of current day and increase amount um 1
     for i in listWithDailyKeystrokes:
         if i not in df:
             df[i] = 0
-        #if i is not in header add to header and increase in last row at this header column ONE. set all other rows to zero
-        #else increase counter in column with same headername by ONE
         
 def keyStrokeCounter():
-    #iterate over columns from daily row
-    #count occurence of current cell object in listWithDailyKeystrokes and add this value to the current cell object
-    '''    
-        execution diagram:
-            1. add row in KeystrokeRecords with daily date as an entry and rest zeros
-            2. get all daily characters 
-            3. if daily chracter does not appear in df add him in header and set whole column to 0
-            4. count occurence of each character from daily keystrokes and add this number to adequate column
-            5. transfer data from daily keystrokecollector to backup file 
-            6. delete daily keystroke collector 
-
-            Next steps: 
-                1. check if it worked (every day correctly)
-                2. send me every sunday an mail with the diagram 
-                    create a several script which i schedule every week on crontab
-                    Every column from this week as an diagram(sum not included but separate as an big number on the top of the diagram)
-                3. clean up code and add a nice read.me
-                4. share with hacker crew.
-    '''
     counter = 2
     for cell in df.iloc[-1,2:]:
         colname = df.columns[counter]
@@ -94,7 +50,6 @@ def keyStrokeCounter():
 
 def KeyboardloggerDataTransfer():
     #transfer data from keystrokeCollector to keystrokecollectorbackup 
-    #function needs to be last one which gets exectuted to ensure to dont delete before i make the analysis
     with open('/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollector.txt','r') as firstfile, open('/Users/maxhager/Projects/Keyboardlogger/LogData/KeystrokeCollectorBackup.txt','a') as secondfile:
         for line in firstfile:   
             secondfile.write(line)
